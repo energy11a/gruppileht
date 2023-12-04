1ac658969ca28fe8c6080c7b6712b11ffcba9f8a
energy11a <kaurlohmus8@gmail.com>
2023-12-04 18:07:35 +0200

    js komentaarid

--- js/meist.js
+++ js/meist.js
@@ -1,15 +1,15 @@
 let slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex);//näitab slaidi
 
-function plusSlides(n) {
+function plusSlides(n) {//annab järgmise slaidi
 	showSlides((slideIndex += n));
 }
 
-function currentSlide(n) {
+function currentSlide(n) {//annab praeguse slaidi
 	showSlides((slideIndex = n));
 }
 
-function showSlides(n) {
+function showSlides(n) {//funktsiooni slaidi vahetamiseks
 	let i;
 	let slides = document.getElementsByClassName("mySlides");
 	let dots = document.getElementsByClassName("dot");
